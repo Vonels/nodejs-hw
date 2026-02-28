b165d97f01c0c6a33aa14fdc481bc76f35c4086b
Vonels <klizka38@gmail.com>
2026-02-28 12:07:55 +0200

    fix

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,3 +1,6 @@
+import createHttpError from 'http-errors';
+import { Note } from '../models/note.js';
+
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
 
@@ -25,4 +28,35 @@ export const getAllNotes = async (req, res) => {
     totalPages: Math.ceil(totalNotes / perPage),
     notes,
   });
+};
+
+export const getNoteById = async (req, res) => {
+  const note = await Note.findById(req.params.noteId);
+
+  if (!note) throw createHttpError(404, 'Note not found');
+
+  res.json(note);
+};
+
+export const createNote = async (req, res) => {
+  const note = await Note.create(req.body);
+  res.status(201).json(note);
+};
+
+export const deleteNote = async (req, res) => {
+  const note = await Note.findByIdAndDelete(req.params.noteId);
+
+  if (!note) throw createHttpError(404, 'Note not found');
+
+  res.json(note);
+};
+
+export const updateNote = async (req, res) => {
+  const note = await Note.findByIdAndUpdate(req.params.noteId, req.body, {
+    new: true,
+  });
+
+  if (!note) throw createHttpError(404, 'Note not found');
+
+  res.json(note);
 };
