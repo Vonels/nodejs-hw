534cbcee9afaf1724297e96f1c637c321f76c95f
Vonels <klizka38@gmail.com>
2026-03-12 18:58:56 +0200

    add library

--- src/server.js
+++ src/server.js
@@ -14,13 +14,13 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cookieParser());
 
 await connectMongoDB();
 
 app.use(logger);
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/notes', notesRoutes);
 
